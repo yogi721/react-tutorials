709197ff1f27bdeb30ff9c8664dd7517ddf53e58
Meh.name Mehdigit config ==global user.email semlali.mehdi.1980@gmail.com <semlali.mehdi.1980@gmail.com>
2021-04-16 00:29:31 +0000

    todo app completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import IndexTyping from './components/typing/IndexTyping';
-import IndexTutorial from './components/tutorial1/IndexTutorial';
+// import IndexTyping from './components/typing/IndexTyping';
+// import IndexTutorial from './components/tutorial1/IndexTutorial';
+import IndexTodo from './components/todo/IndexTodo';
 
 
 function App() {
@@ -7,8 +8,9 @@ function App() {
   return (
     <div className="container mb-5 mb-5">
       <div className="row">
-        <IndexTutorial />
-        <IndexTyping />
+        {/* <IndexTutorial />
+        <IndexTyping /> */}
+        <IndexTodo />
       </div>
     </div>
   );
---
+++ src/components/todo/IndexTodo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import TodoForm from './components/TodoForm'
+import TodoList from './components/TodoList'
+import './todo.css'
+
+function IndexTodo() {
+    return (
+        <div className="todo-app">
+            <TodoList />
+        </div>
+    )
+}
+
+export default IndexTodo
