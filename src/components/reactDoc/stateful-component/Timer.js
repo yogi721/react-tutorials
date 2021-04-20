fde677c2b99103a8dd9698956f8e63a1cc822932
Meh.name Mehdigit config ==global user.email semlali.mehdi.1980@gmail.com <semlali.mehdi.1980@gmail.com>
2021-04-20 22:39:29 +0000

    Timer

--- src/components/reactDoc/stateful-component/Timer.js
+++ src/components/reactDoc/stateful-component/Timer.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
 class Timer extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = { seconds: 0 }
     }
     tick() {
