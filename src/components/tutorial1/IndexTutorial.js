d331493a3d2d19a33dca605269fb4d224a981116
Mehdi <semlali.mehdi.1980@gmail.com>
2021-06-14 02:21:01 +0200

    small change

--- src/components/tutorial1/IndexTutorial.js
+++ src/components/tutorial1/IndexTutorial.js
@@ -4,29 +4,29 @@ import React from 'react'
 // import EventBind from './EventBind';
 // import FunctionClick from './FunctionClick';
 // import Greeting from './Greeting';
-// import ParentComp from './ParentComp';
+import ParentComp from './ParentComp';
 // import UserGreeting from './UserGreeting';
 // import NameList from './ListRendering/NameList';
 // import Stylesheet from './styling/Stylesheet';
 // import Inline from './styling/Inline';
 // import Form from './forms/Form';
-import LifecycleA from './Lifecycle/LifecycleA';
+// import LifecycleA from './Lifecycle/LifecycleA';
 
 
 function IndexTutorial() {
     return (
         <div className="coucou">
-            {/* <Counter />
-            <Greeting name="Mehdi" heroName="speedy man" />
-            <FunctionClick />
-            <EventBind />
+            {/* <Counter /> */}
+            {/* <Greeting name="Mehdi" heroName="speedy man" /> */}
+            {/* <FunctionClick /> */}
+            {/* <EventBind /> */}
             <ParentComp />
-            <UserGreeting /> */}
+            {/* <UserGreeting /> */}
             {/* <NameList /> */}
             {/* <Stylesheet primary={true} /> */}
             {/* <Inline /> */}
             {/* <Form /> */}
-            <LifecycleA />
+            {/* <LifecycleA /> */}
         </div>
     )
 }
